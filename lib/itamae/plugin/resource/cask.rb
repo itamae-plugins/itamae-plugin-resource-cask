d89c82a6a76a9740d3b3d79701c4494d117b8dbf
Takashi Kokubun <takashikkbn@gmail.com>
2014-10-21 20:22:38 +0900

    Bundle gem itamae-plugin-resource-cask

---
+++ lib/itamae/plugin/resource/cask.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/resource/cask/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module Cask
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/resource/cask/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Resource
+      module Cask
+        VERSION = "0.0.1"
+      end
+    end
+  end
+end
