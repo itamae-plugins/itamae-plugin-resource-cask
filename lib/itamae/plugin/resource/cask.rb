1c61954a70f6db381b1666da786caeca7571e50c
Takashi Kokubun <takashikkbn@gmail.com>
2015-09-25 19:25:03 +0900

    Use fullpath for brew

--- lib/itamae/plugin/resource/cask.rb
+++ lib/itamae/plugin/resource/cask.rb
@@ -4,6 +4,8 @@ module Itamae
   module Plugin
     module Resource
       class Cask < Itamae::Resource::Base
+        BREW = '/usr/local/bin/brew'
+
         define_attribute :action, default: :install
         define_attribute :target, type: String, default_name: true
         define_attribute :options, type: String, default: "--appdir=/Applications"
@@ -12,24 +14,24 @@ module Itamae
           super
           ensure_brew_cask_availability
 
-          result = run_command("brew cask list | grep '#{attributes.target}$'", error: false)
+          result = run_command("#{BREW} cask list | grep '#{attributes.target}$'", error: false)
           current.exist = result.exit_status == 0
         end
 
         def action_install(options)
           unless current.exist
-            run_command(["brew", "cask", "install", attributes.target])
+            run_command([BREW, "cask", "install", attributes.target])
           end
         end
 
         def action_alfred(options)
-          run_command(["brew", "cask", "alfred", attributes.target])
+          run_command([BREW, "cask", "alfred", attributes.target])
         end
 
         private
 
         def ensure_brew_cask_availability
-          if run_command("brew cask > /dev/null", error: false).exit_status != 0
+          if run_command("#{BREW} cask > /dev/null", error: false).exit_status != 0
             raise "`brew cask` command is not available. Please install brew cask."
           end
         end
