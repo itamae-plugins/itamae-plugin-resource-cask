1f1372024ade412f19216a454c5934f1c95d300e
Takashi Kokubun <takashikkbn@gmail.com>
2016-10-18 00:58:42 +0900

    Port cask resource for itamae-mruby

---
+++ mrblib/itamae/plugin/resource_executor/cask.rb
@@ -0,0 +1,49 @@
+module Itamae
+  module Plugin
+    module ResourceExecutor
+      class Cask < Itamae::ResourceExecutor::Base
+        def apply(current, desired)
+          ensure_brew_cask_availability
+
+          if !current.exist && desired.exist
+            run_command(["brew", "cask", "install", *Array(desired.options), desired.target])
+          end
+
+          if desired.alfred_linked
+            run_command(["brew", "cask", "alfred", *Array(desired.options), desired.target])
+          end
+        end
+
+        private
+
+        def set_current_attributes(current, action)
+          case action
+          when :install
+            result = run_command("#{brew_cask_list} | grep '#{attributes.target}$'", error: false)
+            current.exist = (result.exit_status == 0)
+          end
+        end
+
+        def set_desired_attributes(desired, action)
+          case action
+          when :install
+            desired.exist = true
+          when :alfred
+            desired.alfred_linked = true
+          end
+        end
+
+        # Optimized `brew cask list`
+        def brew_cask_list
+          "ls -1 /opt/homebrew-cask/Caskroom/"
+        end
+
+        def ensure_brew_cask_availability
+          if run_command("test -d $(brew --prefix)/Library/Taps/caskroom/homebrew-cask", error: false).exit_status != 0
+            raise "`brew cask` command is not available. Please install brew cask."
+          end
+        end
+      end
+    end
+  end
+end
